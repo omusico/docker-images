ce13ae296dd72e9bd49c737b4910ab03288c8966
Phillip Compton <me@philcompton.com>
2015-07-11 15:27:16 -0400

    New MariaDB Image, stll needs persistence help

---
+++ centos-mariadb/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+chown -R mysql:mysql "$DATADIR"
+
+echo 'Running mysql_install_db ...'
+mysql_install_db --datadir="$DATADIR"
+echo 'Finished mysql_install_db'
+
+tempSqlFile='/tmp/mysql-first-time.sql'
+cat > "$tempSqlFile" <<-EOSQL
+	DELETE FROM mysql.user ;
+	CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+	GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+	DROP DATABASE IF EXISTS test ;
+EOSQL
+
+if [ "$MYSQL_DATABASE" ]; then
+	echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
+fi
+
+if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+	echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
+
+	if [ "$MYSQL_DATABASE" ]; then
+		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
+	fi
+fi
+
+echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
+
+exec mysqld --init-file="$tempSqlFile"
